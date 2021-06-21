90d5f9b31413fbcf4468a0ab83c024d8bc0c5516
The Mac Llama <llama@macllama.com>
2021-06-20 20:44:36 -0500

    Add pager settings

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -52,6 +52,11 @@ preexec () {print -Pn "\e]0;%n@%m: $1 (%~)\a"}
 # Set batcat theme
 export BAT_THEME="cobalt2"
 
+# pager settings
+export PAGER="less -RF"
+export BAT_PAGER="less -RF"
+export DELTA_PAGER="less -RF"
+
 # Disable homebrew git token request
 export HOMEBREW_NO_GITHUB_API=TRUE
 
