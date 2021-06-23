87735f06f0b0570e0e97d79cd7b66687d5cc7a67
The couch Llama <llama@couchllama.com>
2021-06-22 19:39:07 -0500

    Fix macos ls

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,12 +1,11 @@
 #!/bin/zsh
 if uname -a | grep -q 'Darwin'; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
-  alias ls="ls -G --group-directories-first"
-
-  if [[ -f /opt/homebrew/bin/ctags ]]; then
-    alias ctags="/opt/homebrew/bin/ctags"
-  else
-    alias ctags="/usr/local/bin/ctags"
+  alias ls="ls -G"
 fi
 
-
+if [[ -f /opt/homebrew/bin/ctags ]]; then
+  alias ctags="/opt/homebrew/bin/ctags"
+else
+  alias ctags="/usr/local/bin/ctags"
+fi
