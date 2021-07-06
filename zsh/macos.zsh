013702d0b93a514343910aafdbee776f58d96c3a
The Couch Llama <85507279+thecouchllama@users.noreply.github.com>
2021-07-06 09:37:33 -0500

    Add support for intel macs

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 if uname -a | grep -q 'Darwin'; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+  if [[ -f /opt/homebrew/bin/brew ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
+  
+  if [[ -f /usr/local/bin/brew ]]; then
+    eval "$(/usr/local/bin/brew shellenv)"
+  fi
+  
   alias ls="ls -G"
 fi
 
