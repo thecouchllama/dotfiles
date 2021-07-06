c45c3b2f83113ecd0a13bfea8f0b25380078609d
The Couch Llama <llama@couchllama.com>
2021-07-05 23:50:54 -0500

    Remove pager for rg

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -7,9 +7,9 @@ alias ping='prettyping --nolegend'
 alias vi=nvim
 
 # Ripgrep Aliases
-alias rg="batgrep --pager='less -R' -S"
-alias rgi="batgrep  --pager='less -R' -i"
-alias rgs="batgrep  --pager='less -R' -s"
+alias rg="batgrep -S"
+alias rgi="batgrep -i"
+alias rgs="batgrep -s"
 
 # Other Aliases
 alias ndu="ncdu --color dark -rr -x --exclude .git --exclude node_modules"
