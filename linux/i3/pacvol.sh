5300bbdf77266b3f48e4ab9353798d69679caecb
The Couch Llama <llama@couchllama.com>
2021-08-20 23:55:45 -0500

    Add archlinux files

---
+++ linux/i3/pacvol.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Copyright (C) 2014 Julien Bonjean <julien@bonjean.info>
+# Copyright (C) 2014 Alexander Keller <github@nycroth.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#------------------------------------------------------------------------
+
+# The second parameter overrides the mixer selection
+# For PulseAudio users, use "pulse"
+# For Jack/Jack2 users, use "jackplug"
+# For ALSA users, you may use "default" for your primary card
+# or you may use hw:# where # is the number of the card desired
+MIXER="pulse"
+
+# The instance option sets the control to report and configure
+# This defaults to the first control of your selected mixer
+# For a list of the available, use `amixer -D $Your_Mixer scontrols`
+SCONTROL="${BLOCK_INSTANCE:-$(amixer -D $MIXER scontrols |
+                  sed -n "s/Simple mixer control '\([A-Za-z ]*\)',0/\1/p" |
+                  head -n1
+                )}"
+
+# The first parameter sets the step to change the volume by (and units to display)
+# This may be in in % or dB (eg. 5% or 3dB)
+STEP="5%"
+
+#------------------------------------------------------------------------
+
+capability() { # Return "Capture" if the device is a capture device
+  amixer -D "$MIXER" get "$SCONTROL" |
+    sed -n "s/  Capabilities:.*cvolume.*/Capture/p"
+}
+
+volume() {
+  amixer -D "$MIXER" get "$SCONTROL" "$(capability)"
+}
+
+#------------------------------------------------------------------------
+
+# Update i3blocks
+pkill -RTMIN+1 i3blocks
+
+case "$1" in
+  toggle) amixer -q -D "$MIXER" sset "$SCONTROL" "$(capability)" toggle ;;  # right click, mute/unmute
+  up) amixer -q -D "$MIXER" sset "$SCONTROL" "$(capability)" "${STEP}+" unmute ;; # scroll up, increase
+  down) amixer -q -D "$MIXER" sset "$SCONTROL" "$(capability)" "${STEP}-" unmute ;; # scroll down, decrease
+  *) echo "Usage: $(basename "$0") [up|down|toggle]"
+    exit 1;;
+esac
---
+++ zsh/archlinux.zsh
@@ -0,0 +1,50 @@
+#!/bin/zsh
+
+if [[ -f "/etc/arch-release" ]]; then
+
+  # Use qt4-ssh-askpass for ssh ask dialog
+  export SSH_ASKPASS="/usr/bin/qt4-ssh-askpass"
+
+  # Arch PKG Management
+  alias pac="sudo pacman -S"
+  alias pacq="pacman -Q"
+  alias pacr="sudo pacman -Rs"
+  alias pacs="pacman -Ss"
+  alias pacsy="sudo pacman -Sy"
+  alias pacsyu="sudo pacman -Syu"
+  alias pacu="sudo pacman -U"
+  alias ya="yay -S"
+  alias yas="yay -Ss"
+  alias yau="yay -Su"
+
+  alias ls="ls --color=always --group-directories-first"
+
+  # BTRFS DF alias
+  alias bdf="btrfs filesystem df"
+
+  # Useful when copying files on a COW-enabled file system
+  alias cp="cp -i --reflink=auto"
+
+  # Other aliases
+  alias open="xdg-open"
+  alias xqf="xdg-mime query filetype"
+  alias xqd="xdg-mime query default"
+  alias xd="xdg-mime default"
+
+  # Ecrypt
+  alias mpriv='ecryptfs-mount-private; cd ~/Private'
+  alias upriv='cd ~; ecryptfs-umount-private'
+
+  # Source linux brew if it's installed
+  if [[ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+      eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  fi
+
+  # Start ssh-agent if it's not already running
+  [ -z "$SSH_AUTH_SOCK" ] && eval "$(ssh-agent -s)"
+
+  if [ ! -S ~/.ssh/ssh_auth_sock ] && [ -S "$SSH_AUTH_SOCK" ]; then
+    ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+  fi
+  SSH_AUTH_SOCK="${HOME}/.ssh/ssh_auth_sock"
+fi
