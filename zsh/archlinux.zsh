a4274f313aa87de83b35345955cd652da3050895
The Couch Llama <llama@couchllama.com>
2021-08-28 23:17:17 -0500

    Update ssh-agent for arch

--- zsh/archlinux.zsh
+++ zsh/archlinux.zsh
@@ -42,9 +42,4 @@ if [[ -f "/etc/arch-release" ]]; then
 
   # Start ssh-agent if it's not already running
   [ -z "$SSH_AUTH_SOCK" ] && eval "$(ssh-agent -s)"
-
-  if [ ! -S ~/.ssh/ssh_auth_sock ] && [ -S "$SSH_AUTH_SOCK" ]; then
-    ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
-  fi
-  SSH_AUTH_SOCK="${HOME}/.ssh/ssh_auth_sock"
 fi
