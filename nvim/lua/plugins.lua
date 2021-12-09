d0c0281331b9883ca89a0f7651f4563d841190aa
The Couch Llama <llama@couchllama.com>
2021-12-08 21:42:18 -0600

    Update cmp mappings

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -339,11 +339,8 @@ cmp.setup({
     mapping = {
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
-      ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.close(),
-      ["<CR>"] = cmp.mapping.confirm {
+      ["<Tab>"] = cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Replace,
         select = true,
       },
