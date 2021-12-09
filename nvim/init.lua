c23d07456de3d439877372143332fefa0b4542d8
The Couch Llama <llama@couchllama.com>
2021-12-08 21:23:44 -0600

    Move gitcommit to its own file

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,5 +3,6 @@ require("options")
 require("statusline")
 require("map")
 
+vim.cmd 'source ~/.config/nvim/vim/git.vim'
 vim.cmd 'source ~/.config/nvim/vim/toggle.vim'
 vim.cmd 'source ~/.config/nvim/vim/colors.vim'
