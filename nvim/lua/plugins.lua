51974afc02435e61c33b3a8145d2e1a24cf2f378
The Couch Llama <llama@couchllama.com>
2021-12-09 22:00:57 -0600

    Add lsp installer

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -43,6 +43,7 @@ require('packer').startup(function()
 
   -- Language Server
   use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
   use 'fatih/vim-go'
 
   -- vim tmux navigator
@@ -379,3 +380,21 @@ require('telescope').load_extension('fzf')
 -- vim-better-whitespace
 g.strip_whitespace_on_save = 1
 g.strip_whitespace_confirm = 0
+
+
+local lsp_installer = require("nvim-lsp-installer")
+
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+lsp_installer.on_server_ready(function(server)
+    local opts = {}
+
+    -- (optional) Customize the options passed to the server
+    -- if server.name == "tsserver" then
+    --     opts.root_dir = function() ... end
+    -- end
+
+    -- This setup() function is exactly the same as lspconfig's setup function.
+    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    server:setup(opts)
+end)
