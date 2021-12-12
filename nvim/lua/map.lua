2700061a20926750a52ea86ec1df65a7673cba8e
The Couch Llama <llama@couchllama.com>
2021-12-11 23:30:48 -0600

    Switch fg to git_files

--- nvim/lua/map.lua
+++ nvim/lua/map.lua
@@ -7,7 +7,7 @@ end
 
 -- telescope
 map('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+map('n', '<leader>fg', '<cmd>Telescope git_files<cr>')
 map('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 
