5d441ab2479a8bb61b1183d032c82973a1d341dd
The Couch Llama <llama@couchllama.com>
2021-12-15 22:36:15 -0600

    Update kitty.zsh

--- zsh/kitty.zsh
+++ zsh/kitty.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-if [ "$TERM" == "xterm-kitty" ]; then
+if [ "$TERM" == "xterm-kitty" ] && [ ! "$TMUX" ]; then
   alias ssh="kitty +kitten ssh"
   alias sshnh='kitty +kitten ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null"'
   alias sshnhr='kitty +kitten ssh -l root -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null"'
-i
+fi
