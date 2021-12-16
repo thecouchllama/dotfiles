7cde721bfaa8f22fdc2d227b777dff74eeea47c3
The Couch Llama <llama@couchllama.com>
2021-12-15 22:45:03 -0600

    Remove kitty.zsh

--- zsh/kitty.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-if [ "$TERM" == "xterm-kitty" ] && [ ! "$TMUX" ]; then
-  alias ssh="kitty +kitten ssh"
-  alias sshnh='kitty +kitten ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null"'
-  alias sshnhr='kitty +kitten ssh -l root -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null"'
-fi
