07fe7cfe801e261e4fe9dfa42647edd0a208b697
The Couch Llama <llama@couchllama.com>
2022-01-01 23:40:29 -0600

    Add more null-ls builtins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -537,19 +537,24 @@ vim.cmd "command! -bang Keymap Telescope keymaps"
 local null_ls = require("null-ls")
 require("null-ls").setup({
   sources = {
-    null_ls.builtins.code_actions.shellcheck,
-    null_ls.builtins.diagnostics.ansiblelint,
-    null_ls.builtins.diagnostics.codespell,
-    null_ls.builtins.diagnostics.golangci_lint,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.diagnostics.vale,
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.fixjson,
-    null_ls.builtins.formatting.golines.with({
-      args = {"--base-formatter=gofumpt"}
-    }),
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.shfmt,
+		null_ls.builtins.code_actions.shellcheck,
+		null_ls.builtins.diagnostics.ansiblelint,
+		null_ls.builtins.diagnostics.codespell,
+		null_ls.builtins.diagnostics.golangci_lint,
+    null_ls.builtins.diagnostics.jsonlint,
+    null_ls.builtins.diagnostics.markdownlint,
+		null_ls.builtins.diagnostics.shellcheck,
+		null_ls.builtins.diagnostics.vale,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.codespell,
+		null_ls.builtins.formatting.fixjson,
+		null_ls.builtins.formatting.golines.with({
+			args = { "--base-formatter=gofumpt" },
+		}),
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.shfmt,
+		null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.taplo,
   },
   -- you can reuse a shared lspconfig on_attach callback here
   on_attach = function(client)
