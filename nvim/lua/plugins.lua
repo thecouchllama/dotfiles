4ef4f9da3de5fd6fa258b50382e5d5ac09563109
The Couch Llama <llama@couchllama.com>
2022-01-15 23:34:35 -0600

    Add friendly-snippets to luasnip path

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -348,7 +348,7 @@ require("diffview").setup({
 	},
 })
 
-require("luasnip/loaders/from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+require("luasnip/loaders/from_vscode").lazy_load({ paths = vim.fn.stdpath("config") .. "/snippets,~/.local/share/nvim/site/pack/packer/start/friendly-snippets"})
 local luasnip = require("luasnip")
 
 local t = function(str)
