e814e746f01510e9b723aa2a638f9c153038dd50
The Couch Llama <llama@couchllama.com>
2022-01-20 21:11:33 -0600

    Fix diff function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -13,5 +13,5 @@ function catnc() {
 # uses batcat for diff. This is done in a function since columns isn't working
 # properly in blink shell
 function diff() {
-  batdiff --delta --terminal-width=$(($COLUMNS-1))
+  batdiff --delta --terminal-width=$(($COLUMNS-1)) $1 $2
 }
