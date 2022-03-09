bbff0eb7ae31a4d1d356e792542f223ca7fd92e1
The Couch Llama <llama@couchllama.com>
2022-03-08 21:46:30 -0600

    Add maxdepth

--- linux/rofi/rofi-git.sh
+++ linux/rofi/rofi-git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-selector=$(find ~/git -type d -name '.git' | sed 's|/.git||' | sed "s.$HOME\/git\/.." | rofi -dmenu -p "> " -mesg "Git Repos")
+selector=$(find ~/git -maxdepth 3 -type d -name '.git' | sed 's|/.git||' | sed "s.$HOME\/git\/.." | rofi -dmenu -p "> " -mesg "Git Repos")
 
 code ${HOME}/git/$selector
