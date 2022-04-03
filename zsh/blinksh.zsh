9db9ab0e23db9591b2fea2a4febab61276509395
The Couch Llama <llama@couchllama.com>
2022-04-02 22:10:51 -0500

    Add remote usable zsh setup

---
+++ zsh/blinksh.zsh
@@ -0,0 +1,11 @@
+if [[ "$BLINKSH" ]]; then
+  ZLE_RPROMPT_INDENT=0
+
+  # Start ssh-agent if it's not already running
+  [ -z "$SSH_AUTH_SOCK" ] && eval "$(ssh-agent -s -t 1h)"
+
+  if [ ! -S ~/.ssh/ssh_auth_sock ] && [ -S "$SSH_AUTH_SOCK" ]; then
+    ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+  fi
+  SSH_AUTH_SOCK="${HOME}/.ssh/ssh_auth_sock"
+fi
