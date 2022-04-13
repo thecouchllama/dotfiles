5def08d6ed80d7cb5412c467cdd37d58edd2669e
The Couch Llama <llama@couchllama.com>
2022-04-12 22:24:34 -0500

    Attempt to fix macos script

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,11 +1,7 @@
-#!/bin/zsh
 if [[ -d /Users ]]; then
   if [[ -f /opt/homebrew/bin/brew ]]; then
-    export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
     eval "$(/opt/homebrew/bin/brew shellenv)"
-  fi
-
-  if [[ -f /usr/local/bin/brew ]]; then
+  elif [[ -f /usr/local/bin/brew ]]; then
     eval "$(/usr/local/bin/brew shellenv)"
   fi
 
