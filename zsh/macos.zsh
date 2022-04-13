f4f132f8507f2404e3445c1c776638aa999b5e12
The Couch Llama <llama@couchllama.com>
2022-04-12 22:26:37 -0500

    Simplify macos zsh script

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,15 +1,5 @@
 if [[ -d /Users ]]; then
-  if [[ -f /opt/homebrew/bin/brew ]]; then
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-  elif [[ -f /usr/local/bin/brew ]]; then
-    eval "$(/usr/local/bin/brew shellenv)"
-  fi
-
-  alias ls="ls -G"
-
-  if [[ -f /opt/homebrew/bin/ctags ]]; then
-    alias ctags="/opt/homebrew/bin/ctags"
-  else
-    alias ctags="/usr/local/bin/ctags"
-  fi
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+	alias ls="ls -G"
+	alias ctags="/opt/homebrew/bin/ctags"
 fi
