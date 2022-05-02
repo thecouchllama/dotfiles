fef2e7c9a82d8c51a272fe02a71f1f226d198018
The Couch Llama <llama@couchllama.com>
2022-05-02 13:14:46 -0500

    Add support for intel macs

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,5 +1,12 @@
+#!/bin/zsh
 if [[ -d /Users ]]; then
-	eval "$(/opt/homebrew/bin/brew shellenv)"
 	alias ls="ls -G"
+  if [ -f "/opt/homebrew/bin/brew" ]; then
+	eval "$(/opt/homebrew/bin/brew shellenv)"
 	alias ctags="/opt/homebrew/bin/ctags"
+elif [ -f "/usr/local/bin/brew" ]; then
+	eval "$(/usr/local/bin/brew shellenv)"
+	alias ctags="/usr/local/bin/ctags"
+  fi
+
 fi
