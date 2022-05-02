005b4b4e8f4226d935124718760f2904600d787d
The Couch Llama <llama@couchllama.com>
2022-05-02 13:43:52 -0500

    Add barbar files

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,5 +7,6 @@ require("toggle_cmp")
 vim.cmd("source ~/.config/nvim/vim/fzf.vim")
 vim.cmd("source ~/.config/nvim/vim/git.vim")
 vim.cmd("source ~/.config/nvim/vim/toggle.vim")
+vim.cmd("source ~/.config/nvim/vim/colors.vim")
 
 require("local")
