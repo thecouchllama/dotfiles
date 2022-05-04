7bb1be5b131e1d0cb07302f0fabfccaaa23c0079
The Couch Llama <llama@couchllama.com>
2022-05-03 23:21:38 -0500

    Update blinksh script

--- zsh/blinksh.zsh
+++ zsh/blinksh.zsh
@@ -1,10 +1,9 @@
 if [[ "$BLINKSH" ]]; then
   ZLE_RPROMPT_INDENT=0
 
-  # Start ssh-agent if it's not already running
-  [ -z "$SSH_AUTH_SOCK" ] && eval "$(/usr/bin/ssh-agent -s -t 1h)"
-
   if [ ! -S ~/.ssh/ssh_auth_sock ] && [ -S "$SSH_AUTH_SOCK" ]; then
+    # Start ssh-agent if it's not already running
+    [ -z "$SSH_AUTH_SOCK" ] && eval "$(/usr/bin/ssh-agent -s -t 1h)"
     /usr/bin/ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
   fi
   SSH_AUTH_SOCK="${HOME}/.ssh/ssh_auth_sock"
