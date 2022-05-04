7434e019fb6916d87dc1817d00184d51bbf168b4
The Couch Llama <llama@couchllama.com>
2022-05-03 23:24:34 -0500

    Fix if statement

--- zsh/blinksh.zsh
+++ zsh/blinksh.zsh
@@ -1,7 +1,7 @@
 if [[ "$BLINKSH" ]]; then
   ZLE_RPROMPT_INDENT=0
 
-  if [ ! -S ~/.ssh/ssh_auth_sock ] && [ -S "$SSH_AUTH_SOCK" ]; then
+  if [ ! -S ~/.ssh/ssh_auth_sock ]; then
     # Start ssh-agent if it's not already running
     [ -z "$SSH_AUTH_SOCK" ] && eval "$(/usr/bin/ssh-agent -s -t 1h)"
     /usr/bin/ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
