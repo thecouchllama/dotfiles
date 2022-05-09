be51c9db43b56190e487bd571e7f238f7c18c3d3
The Couch Llama <llama@couchllama.com>
2022-05-08 21:29:06 -0500

    Remove if statement

--- zsh/doing.zsh
+++ zsh/doing.zsh
@@ -2,11 +2,9 @@
 
 # Doing Aliases
 # https://github.com/ttscoff/doing
-if which doing >/dev/null; then
-  alias dn="doing now"
-  alias ddone="doing done"
-  alias dl="doing later"
-  alias dnext="doing done; doing now"
-  alias dt="doing today --totals | colout '^([ \d:apm]+) ?([>:]) (.*)' green,black,white"
-  alias dy="doing yesterday --totals | colout '^([ \d:apm]+) ?([>:]) (.*)' green,black,white"
-fi
+alias dn="doing now"
+alias ddone="doing done"
+alias dl="doing later"
+alias dnext="doing done; doing now"
+alias dt="doing today --totals | colout '^([ \d:apm]+) ?([>:]) (.*)' green,black,white"
+alias dy="doing yesterday --totals | colout '^([ \d:apm]+) ?([>:]) (.*)' green,black,white"
