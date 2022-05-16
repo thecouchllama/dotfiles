5d0168dde2fccfc79549166e21450d85b0d6eaa7
The Couch Llama <llama@couchllama.com>
2022-05-15 22:48:08 -0500

    Update doing

--- zsh/doing.zsh
+++ zsh/doing.zsh
@@ -4,7 +4,8 @@
 # https://github.com/ttscoff/doing
 alias dn="doing now"
 alias ddone="doing done"
-alias dl="doing later"
+alias dl="doing last"
 alias dnext="doing done; doing now"
 alias dt="doing today --totals"
 alias dy="doing yesterday --totals"
+alias dw='doing show --from "sat to fri" --times --totals'
