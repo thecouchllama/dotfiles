186f27bc5fd6346edf7941f612d4916de730c710
The Couch Llama <llama@couchllama.com>
2022-06-25 21:10:02 -0500

    Update blinksh zsh

--- zsh/blinksh.zsh
+++ zsh/blinksh.zsh
@@ -4,7 +4,11 @@ if [[ "$BLINKSH" ]]; then
   if [ ! -S ~/.ssh/ssh_auth_sock ]; then
     # Start ssh-agent if it's not already running
     [ -z "$SSH_AUTH_SOCK" ] && eval "$(/usr/bin/ssh-agent -s -t 1h)"
-    /usr/bin/ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+    if [ -f /usr/bin/ls ]; then
+      /usr/bin/ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+    else
+      /bin/ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+    fi
   fi
   export SSH_AUTH_SOCK="${HOME}/.ssh/ssh_auth_sock"
 fi
