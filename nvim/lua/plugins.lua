e0c1537a6d50427ac3e4a178225157fb6a688471
The Couch Llama <llama@couchllama.com>
2022-07-06 23:02:25 -0500

    Update treesitter config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -216,16 +216,15 @@ require("gitsigns").setup({
 
 require("nvim-treesitter.configs").setup({
 	on_config_done = nil,
-	ensure_installed = {}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = "all",
+	sync_install = false,
 	ignore_install = {},
 	matchup = {
-		enable = false, -- mandatory, false will disable the whole extension
-		-- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
+		enable = false,
 	},
 	highlight = {
-		enable = true, -- false will disable the whole extension
+		enable = true,
 		additional_vim_regex_highlighting = true,
-		disable = { "latex" },
 	},
 	context_commentstring = {
 		enable = false,
@@ -235,7 +234,6 @@ require("nvim-treesitter.configs").setup({
 	incremental_selection = {
 		enable = enable,
 		keymaps = {
-			-- mappings for incremental selection (visual mappings)
 			init_selection = "gnn", -- maps in normal mode to init the node/scope selection
 			node_incremental = "grn", -- increment to the upper named parent
 			scope_incremental = "grc", -- increment to the upper scope (as defined in locals.scm)
@@ -243,7 +241,6 @@ require("nvim-treesitter.configs").setup({
 		},
 	},
 	textobjects = {
-		-- syntax-aware textobjects
 		enable = enable,
 		lsp_interop = {
 			enable = enable,
@@ -254,10 +251,8 @@ require("nvim-treesitter.configs").setup({
 		},
 		keymaps = {
 			["iL"] = {
-				-- you can define your own textobjects directly here
 				go = "(function_definition) @function",
 			},
-			-- or you use the queries from supported languages with textobjects.scm
 			["af"] = "@function.outer",
 			["if"] = "@function.inner",
 			["aC"] = "@class.outer",
@@ -276,7 +271,7 @@ require("nvim-treesitter.configs").setup({
 		},
 		move = {
 			enable = enable,
-			set_jumps = true, -- whether to set jumps in the jumplist
+			set_jumps = true,
 			goto_next_start = {
 				["]m"] = "@function.outer",
 				["]]"] = "@class.outer",
@@ -297,12 +292,10 @@ require("nvim-treesitter.configs").setup({
 		select = {
 			enable = enable,
 			keymaps = {
-				-- You can use the capture groups defined in textobjects.scm
 				["af"] = "@function.outer",
 				["if"] = "@function.inner",
 				["ac"] = "@class.outer",
 				["ic"] = "@class.inner",
-				-- Or you can define your own textobjects like this
 				["iF"] = {
 					python = "(function_definition) @function",
 					cpp = "(function_definition) @function",
