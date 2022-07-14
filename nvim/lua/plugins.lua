875437edec3ff33d61d192362f4a370c9ee48856
The Couch Llama <llama@couchllama.com>
2022-07-14 13:21:17 -0500

    Remove unused treesitter options

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -241,9 +241,6 @@ require("nvim-treesitter.configs").setup({
   ensure_installed = "all",
   sync_install = false,
   ignore_install = {},
-  matchup = {
-    enable = false,
-  },
   context_commentstring = {
     enable = true,
   },
@@ -334,28 +331,6 @@ require("nvim-treesitter.configs").setup({
       swap_previous = {
         ["<leader>A"] = "@parameter.inner",
       },
-    },
-  },
-  textsubjects = {
-    enable = false,
-    keymaps = { ["."] = "textsubjects-smart", [";"] = "textsubjects-big" },
-  },
-  playground = {
-    enable = false,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = "o",
-      toggle_hl_groups = "i",
-      toggle_injected_languages = "t",
-      toggle_anonymous_nodes = "a",
-      toggle_language_display = "I",
-      focus_language = "f",
-      unfocus_language = "F",
-      update = "R",
-      goto_node = "<cr>",
-      show_help = "?",
     },
   },
   rainbow = {
