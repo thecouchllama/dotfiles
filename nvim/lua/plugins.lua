dd6b105675fa7ade8c864c317f97a9333d302ddd
The Couch Llama <llama@couchllama.com>
2022-07-15 22:39:43 -0500

    Fix gopls path

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -593,7 +593,7 @@ local path = require("nvim-lsp-installer.core.path")
 local install_root_dir = path.concat({ vim.fn.stdpath("data"), "lsp_servers" })
 
 require("go").setup({
-  gopls_cmd = { install_root_dir .. "/go/gopls" },
+  gopls_cmd = { install_root_dir .. "/gopls/gopls" },
   fillstruct = "gopls",
   lsp_gofumpt = true,
   dap_debug = true,
